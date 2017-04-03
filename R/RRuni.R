c304d53dd21866c31115feca13185a03b22ec566
danheck <danheck@web.de>
2017-04-03 13:41:21 +0200

    RRuni() bugfix: solve Hessian

--- R/RRuni.R
+++ R/RRuni.R
@@ -111,6 +111,7 @@ RRuni <- function(response, data, model, p, group = NULL, MLest=TRUE){
     # optimization
     oo <- optim(start, fn=RRuni.ll, method="L-BFGS-B", lower=1e-8, upper=1-1e-8, hessian=T,
                 model=model, response=response, pp=p, group=group, ncat=ncat)
+    SE <- rep(NA, length(oo$par))
     try(SE <- sqrt(diag(solve(oo$hessian))))
     
     res$pi <- oo$par[1] 
